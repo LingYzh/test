8b872f4a4ce992b0b265291fc40efe2f8e5c52fd
LingYzh <87362804+LingYzh@users.noreply.github.com>
2021-09-10 11:15:47 +0800

    commit on 'master'

---
+++ src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>9999 {{t_data}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        props: ['t_data'],
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: #6cf;
+    }
+</style>
